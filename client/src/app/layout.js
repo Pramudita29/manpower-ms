4b92bf157e0806740ab1d9d4d41f5c58939754d4
Pramudita <pbhattarai0129@gmail.com>
2026-02-01 15:37:57 +0545

    add socket.io and socket.io-client dependencies; update layout for global notifications

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,31 +1,34 @@
 // client/src/app/layout.js
-"use client"; // We need this to handle the global interceptor
+"use client";
 import axios from 'axios';
 import Cookies from 'js-cookie';
 import { useEffect } from 'react';
+import { Toaster } from 'react-hot-toast'; // Import a global toaster
 import '../app/global.css';
 
 export default function RootLayout({ children }) {
     useEffect(() => {
-        // Global Interceptor: If any API call returns 401 (Unauthorized), logout immediately
         const interceptor = axios.interceptors.response.use(
             (response) => response,
             (error) => {
                 if (error.response && error.response.status === 401) {
                     localStorage.clear();
                     Cookies.remove('token', { path: '/' });
-                    window.location.href = '/login'; // Nuclear redirect
+                    window.location.href = '/login';
                 }
                 return Promise.reject(error);
             }
         );
-
         return () => axios.interceptors.response.eject(interceptor);
     }, []);
 
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body>
+                {/* This allows notifications to pop up on ANY page */}
+                <Toaster position="top-right" />
+                {children}
+            </body>
         </html>
     );
 }
